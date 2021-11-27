be4cd9f5d29796f8642bf2b2cadc1bc6d39bfadf
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2021-11-27 11:41:18 -0500

    Add files via upload

---
+++ bir kelime büyük bir kelime kücük nasıl yapılır/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace bir_kelime_büyük_bir_kelime_kücük_nasıl_yapılır
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StringBuilder EBSsb = new StringBuilder();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EBSTime(textBox1.Text,textBox2);
+            
+        }
+
+        private void EBSTime(string cml, TextBox tex)
+        {
+          
+            bool uppercase = false;
+            foreach (char c in cml)
+            {
+                if (uppercase)
+                    EBSsb.Append(char.ToUpper(c));
+                else
+                    EBSsb.Append(char.ToLower(c));
+
+                uppercase = !uppercase;
+            }
+
+            tex.Text = EBSsb.ToString();
+        }
+    }
+}
